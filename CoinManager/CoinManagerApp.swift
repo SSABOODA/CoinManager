7b75189bd8f0cbaeb2c718bf56eb992c5f42fd2a
SSABOODA <ssaboo92@gmail.com>
2024-01-02 21:22:23 +0900

    Initial Commit

---
+++ CoinManager/CoinManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManagerApp.swift
+//  CoinManager
+//
+//  Created by 한성봉 on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
